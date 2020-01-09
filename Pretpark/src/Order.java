9530796526f8a127a35d5ebbe3972dd578549b49
Ruben <rubenkuilder@gmail.com>
2020-01-09 21:40:29 +0100

    Added fields to ParkManagement class.

--- Pretpark/src/Order.java
+++ Pretpark/src/Order.java
@@ -1,2 +1,7 @@
 public class Order {
+    public int orderId;
+
+    public Order(int orderId) {
+        this.orderId = orderId;
+    }
 }
--- Pretpark/src/ParkManagement.java
+++ Pretpark/src/ParkManagement.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class ParkManagement {
+    private ArrayList<Order> orderList;
+    private ArrayList<Attraction> attractionList;
+    private double dayRevenue;
+    private int totalDayVisitors;
+    private int totalDayElderlyVisitors;
+
+    public ParkManagement() {
+        orderList = new ArrayList<>();
+        attractionList = new ArrayList<>();
+    }
+
+    public void addOrder(int orderId) {
+        orderList.add(new Order(orderId));
+    }
 }
