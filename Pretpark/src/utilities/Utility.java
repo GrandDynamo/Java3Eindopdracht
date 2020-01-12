ba90b80bf802490422d3d827bf2aa631ab501f93
GrandDynamo <43609220+GrandDynamo@users.noreply.github.com>
2020-01-12 20:32:17 +0100

    removed unnecessary method
    ruben dikke dyslekt dat je bent

--- Pretpark/src/utilities/Utility.java
+++ Pretpark/src/utilities/Utility.java
@@ -2,10 +2,6 @@ package utilities;
 
 public class Utility {
     public static double roundTwoDecimal(double number){
-        double roundoff = Math.round(number * 100) / 100D;
-        return roundoff;
-    }
-    public static String generateUniqueID(String typeCode){
-        return typeCode + System.currentTimeMillis();
+        return Math.round(number * 100) / 100D;
     }
 }
